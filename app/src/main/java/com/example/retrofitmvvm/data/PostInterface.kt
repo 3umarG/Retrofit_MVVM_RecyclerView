1f53a33a9d7a44f507c1b35558c72672d7d622cf
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-30 22:38:33 +0200

    Handle Dynamic GET Resource Commit

--- app/src/main/java/com/example/retrofitmvvm/data/PostInterface.kt
+++ app/src/main/java/com/example/retrofitmvvm/data/PostInterface.kt
@@ -3,8 +3,13 @@ package com.example.retrofitmvvm.data
 import com.example.retrofitmvvm.model.PostModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PostInterface {
     @GET("posts")
     fun getPosts() : Call<List<PostModel>>
+
+
+    @GET("posts/{id}")
+    fun getSinglePost(@Path("id") id : Int) : Call<PostModel>
 }
